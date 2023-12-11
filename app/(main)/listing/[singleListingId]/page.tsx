0a450c2987138c382a09b65e6f7a3e0c52dce753
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-12-11 14:09:56 +0300

    Create new routes and nested routes

---
+++ app/(main)/listing/[singleListingId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingeListingPage = () => {
+  return (
+    <div>SingeListingPage</div>
+  )
+}
+
+export default SingeListingPage
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <h1>Home Hub</h1>
-    </main>
-  )
-}
