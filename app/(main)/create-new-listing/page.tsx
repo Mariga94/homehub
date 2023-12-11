164bfb3377c9ce95d9a65e0bb1c3e0c011d83550
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-12-11 18:36:55 +0300

    Remove next js

--- app/(main)/create-new-listing/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CreateNewListing = () => {
-  return (
-    <div>CreateNewListing</div>
-  )
-}
-
-export default CreateNewListing
--- app/(main)/listing/[singleListingId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SingeListingPage = () => {
-  return (
-    <div>SingeListingPage</div>
-  )
-}
-
-export default SingeListingPage
