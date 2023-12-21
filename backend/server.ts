3102ba4bd4c474aff9addddb83254398e5423400
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-12-21 15:38:24 +0300

    Create connection to single  mongodb database using mongoose.connect() method

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,24 @@
 import express from 'express';
+import * as mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
 const app = express();
+const PORT = process.env.port || 3001;
+const MONGODB_URI: string = process.env.MONGO_URL!;
+
+function connectToDB() {
+    try {
+        mongoose.connect(MONGODB_URI)
+        console.log('connected to mongodb');
+    } catch (error: any) {
+        console.error('Error connecting to MONGODB', error.message)
+    }
+}
 
 app.listen(3000, () => {
+    connectToDB()
     console.log("Server listening on port 3000")
 })
