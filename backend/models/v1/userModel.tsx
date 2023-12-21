b330f2696325930fa25b83b8d2079f604b8fc9f1
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-12-21 16:17:38 +0300

    feat create user model

---
+++ backend/models/v1/userModel.tsx
@@ -0,0 +1,21 @@
+import { Schema, Document, model } from "mongoose";
+
+// Define the interface for the User document
+interface IUser extends Document {
+  fullName: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+}
+
+// Define the User schema
+const userSchema = new Schema<IUser>({
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const UserModel = model<IUser>("User", userSchema);
+
+export default UserModel;
