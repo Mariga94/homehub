767911dbff097e94abf1a17c86be9fb21ee6281a
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-12-22 14:30:55 +0300

    Feat Add api documenation using swagger

--- backend/routes/v1/propertyRoutes.ts
+++ backend/routes/v1/propertyRoutes.ts
@@ -3,6 +3,40 @@ import * as PropertyController from '../../controllers/v1/propertyControllers';
 import authenticateUser from '../../middlewares/authMiddleware'
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *  name: Properties
+ *  description: API endpoints for managing properties
+ */
+
+/**
+ * @swagger
+ * /api/property:
+ *   post:
+ *     summary: "Create a new Property"
+ *     description: "Create a new property with the provided details"
+ *     tags:
+ *       - "Properties"
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       description: "Property details"
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/PropertyInput'
+ *     responses:
+ *       200:
+ *         description: "Successfully created a new property"
+ *       401:
+ *         description: "Unauthorized, user not authenticated"
+ *       500:
+ *         description: "Internal Server Error"
+ */
+
+
 router.post('/', authenticateUser, PropertyController.createProperty);
 router.get('/', authenticateUser, PropertyController.getProperties);
 router.get('/:id', authenticateUser, PropertyController.getPropertyById);
--- backend/server.ts
+++ backend/server.ts
@@ -1,3 +1,4 @@
+// Import required modules and libraries
 import express from 'express';
 import * as mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -5,14 +6,18 @@ import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser';
 import authRoutes from './routes/v1/authRoutes';
 import propertyRoutes from './routes/v1/propertyRoutes';
+import { swaggerUi, specs } from './swagger'
 
+// Load environment variables from .env file
 dotenv.config();
 
+// Create an Express app
 const app = express();
 
 const PORT = process.env.port || 3001;
 const MONGODB_URI: string = process.env.MONGO_URL!;
 
+// function to connect to MONGODB
 function connectToDB() {
     try {
         mongoose.connect(MONGODB_URI)
@@ -22,11 +27,18 @@ function connectToDB() {
     }
 }
 
-app.use(bodyParser.json())
-app.use(cookieParser())
+// Middleware setup
+app.use(bodyParser.json()) //Parse JSON bodies in requests
+app.use(cookieParser())  //Parse cookies in request
+
+// Swagger documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
+
+// API routes setup
 app.use('/api/auth', authRoutes);
 app.use('/api/property', propertyRoutes)
 
+// Default route for testin server conncetion.
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'connected' })
 })
@@ -36,4 +48,5 @@ app.listen(PORT, () => {
     console.log("Server listening on port 3000")
 })
 
+// Export the Express app for testing
 export default app;
