aad603a8aebfdd0e88acbad659473466f288c929
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2024-01-13 03:19:58 +0300

    Feat: Add new features to the home page

--- client/src/_root/pages/Home.tsx
+++ client/src/_root/pages/Home.tsx
@@ -1,163 +1,186 @@
 import TopBar from "@/components/shared/TopBar";
 import Footer from "@/components/shared/Footer";
-import { SearchForm } from "@/components/forms/SearchForm";
-import { Bed, Bath } from "lucide-react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+// import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { fetchData } from "@/services/api";
+import PropertyCard from "@/components/shared/PropertyCard";
+import { PropertyInterface } from "@/_dashboard/_components/ListingTable";
+import { useNavigate, useLocation, Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+
 const Home = () => {
+  const [latestPropertiesForSale, setLatestPropertiesForSale] = useState<
+    PropertyInterface[]
+  >([]);
+  const [latestPropertiesForRent, setLatestPropertiesForRent] = useState<
+    PropertyInterface[]
+  >([]);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const currentUrl = location.pathname;
+  const handleOnClick = (id: string) => {
+    navigate(`${currentUrl}/${id}`);
+  };
+  useEffect(() => {
+    const fetchLatestPropertiesForSale = async () => {
+      try {
+        const res = await fetchData("property/latest/for-sale");
+        setLatestPropertiesForSale(res.properties);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchLatestPropertiesForSale();
+    const fetchLatestPropertiesForRent = async () => {
+      try {
+        const res = await fetchData("property/latest/for-rent");
+        setLatestPropertiesForRent(res.properties);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchLatestPropertiesForRent();
+  }, []);
   return (
     <div className="w-full">
       <header className=" relative overflow-hidden bg-no-repeat bg-custom-image bg-cover bg-center h-screen">
         <div className="absolute bg-fixed w-full bg-black bg-opacity-60 h-full text-white">
           <TopBar />
-          <div className="flex flex-col items-center lg:mt-40 mt-28">
-            <h1 className="text-[#EDEFF6]">Find Your Dream Home</h1>
+          <div className="flex  flex-col items-center mt-20 lg:mt-40 ">
+            <h1 className="text-[#EDEFF6] w-full text-center">
+              Buy, rent or sell your property easily.
+            </h1>
             <p className="text-[#EDEFF6] mt-6 lg:w-[43rem] text-center">
               Discover comfort, joy, and a life well-lived. Welcome home to the
               dreams that await you.
             </p>
+            <p className="text-[#EDEFF6] mt-4 lg:w-[43rem] text-center">
+              We give a great platform to buy, sell or even rent your properties
+              without any commissions.
+            </p>
           </div>
-          {/* <div className="mt-32 flex items-center justify-center">
-            <SearchForm />
-          </div> */}
+          <div className="lg:hidden md:hidden flex flex-col gap-5 mt-14 items-center justify-center">
+        
+            <div className="gap-5 flex flex-row">
+              <Button
+                variant="outline"
+                asChild
+                className={cn("text-sm ", "text-gray-500", "w-40")}
+              >
+                <Link to="/listing/for-sale">Sale</Link>
+              </Button>
+              <Button
+                variant="outline"
+                asChild
+                className={cn("text-gray-500 w-40")}
+              >
+                <Link to="/listing/for-rent">Rent</Link>
+              </Button>
+            </div>
+          </div>
         </div>
       </header>
       {/* Why Choose us section */}
       <section className="flex flex-col px-10 lg:px-20 py-10 lg:py-24 gap-10 items-center justify-center bg-slate-100">
-        {/* <h3 className="text-blue-600">Our Expertise</h3> */}
         <h2 className="">Why Choose Us</h2>
         <section className="flex flex-wrap gap-10 justify-center items-center">
           <Card className="lg:w-1/5">
             <CardHeader>
               <CardTitle>Apartments</CardTitle>
-              <CardDescription>Card Description</CardDescription>
             </CardHeader>
             <CardContent>
               <p className="text-justify">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Accusamus modi id quam illo voluptates quisquam fuga, laboriosam
-                cupiditate nam dolorum eius, atque itaque dicta mollitia ratione
-                tempora, necessitatibus aspernatur quod?
+                Our mission is to simplify the apartment hunting process and
+                empower you to find the perfect living space that suits your
+                needs and lifestyle. We understand that finding the right
+                apartment can be a challenging task, and we're here to make the
+                process smooth, enjoyable, and efficient.
               </p>
             </CardContent>
           </Card>
           <Card className="lg:w-1/5">
             <CardHeader>
-              <CardTitle>Family Homes</CardTitle>
-              <CardDescription>Card Description</CardDescription>
+              <CardTitle>Homes</CardTitle>
             </CardHeader>
             <CardContent>
               <p className="text-justify">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Accusamus modi id quam illo voluptates quisquam fuga, laboriosam
-                cupiditate nam dolorum eius, atque itaque dicta mollitia ratione
-                tempora, necessitatibus aspernatur quod?
+                We are committed to simplifying the journey of finding your
+                dream home. We understand that a home is more than just a place;
+                it's where memories are created and stories unfold. Our mission
+                is to guide you through this significant life decision with
+                expertise and care. Key Features
               </p>
             </CardContent>
           </Card>
           <Card className="lg:w-1/5 w-full">
             <CardHeader>
               <CardTitle>Pricing</CardTitle>
-              <CardDescription>Card Description</CardDescription>
             </CardHeader>
             <CardContent>
               <p className="text-justify">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Accusamus modi id quam illo voluptates quisquam fuga, laboriosam
-                cupiditate nam dolorum eius, atque itaque dicta mollitia ratione
-                tempora, necessitatibus aspernatur quod?
+                Transparency is our priority. We believe in providing a clear
+                and fair pricing structure to ensure that you have all the
+                information you need to make informed decisions. we are
+                committed to offering pricing that is competitive,
+                straightforward, and aligned with the value we provide to our
+                users. Whether you are a homebuyer, seller, or real estate
+                professional, our pricing model is designed to meet your needs.
               </p>
             </CardContent>
           </Card>
           <Card className="lg:w-1/5">
             <CardHeader>
               <CardTitle>Commercial</CardTitle>
-              <CardDescription>Card Description</CardDescription>
             </CardHeader>
             <CardContent>
               <p className="text-justify">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Accusamus modi id quam illo voluptates quisquam fuga, laboriosam
-                cupiditate nam dolorum eius, atque itaque dicta mollitia ratione
-                tempora, necessitatibus aspernatur quod?
+                Our vision is to revolutionize the way businesses find and
+                secure commercial spaces. We understand the unique requirements
+                of commercial real estate, and our platform is tailored to
+                simplify the process of acquiring the perfect space for your
+                business needs.
               </p>
             </CardContent>
           </Card>
         </section>
       </section>
 
-      {/* <section className="flex flex-col px-10 lg:px-20 py-10 lg:py-24 m-auto gap-10 items-center justify-center bg-slate-100">
+      <section className="flex flex-col px-10 lg:px-20 py-10 lg:py-24 m-auto gap-10 items-center justify-center bg-slate-100">
         <h2>Latest House on Sale</h2>
         <p className="text-[#889099] text-center">
           Checkout latest houses, apartment and commercial properties on sale.
         </p>
         <section className="flex gap-4 container overflow-x-auto hide-scroll-bar ">
-          <Card className={cn("w-[350px] snap-center flex-shrink-0")}>
-            <CardHeader>
-              <CardTitle>
-                <img src="/public/images/jumbotron.jpg" alt="" />
-              </CardTitle>
-              <CardDescription>
-                <h3 className="text-blue-600 leading-6">
-                  3-Bedroom Furnished Duplex Apartment For Rent in Kilimani
-                </h3>
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <span className="font-semibold">Kes 250,000 / month</span>
-              <div className="flex flex-row gap-5 text-[#889099]">
-                <div className="flex flex-row gap-1 items-center justify-center ">
-                  <Bed />
-                  <span>3</span> beds
-                </div>
-                <div className="flex flex-row gap-1 items-center justify-center">
-                  <Bath />
-                  <span>2</span> baths
-                </div>
-              </div>
-            </CardContent>
-            <CardFooter>
-              <p>Kilimani, Nairobi, Kenya</p>
-            </CardFooter>
-          </Card>
-          <Card className={cn("w-[350px] snap-center flex-shrink-0")}>
-            <CardHeader>
-              <CardTitle>
-                <img src="/public/images/jumbotron.jpg" alt="" />
-              </CardTitle>
-              <CardDescription>
-                <h3 className="text-blue-600 text-2xl">
-                  3-Bedroom Furnished Duplex Apartment For Rent in Kilimani
-                </h3>
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <span className="font-semibold">Kes 250,000 / month</span>
-              <div className="flex flex-row gap-5 text-[#889099]">
-                <div className="flex flex-row gap-1 items-center justify-center ">
-                  <Bed />
-                  <span>3</span> beds
-                </div>
-                <div className="flex flex-row gap-1 items-center justify-center">
-                  <Bath />
-                  <span>2</span> baths
-                </div>
-              </div>
-            </CardContent>
-            <CardFooter>
-              <p>Kilimani, Nairobi, Kenya</p>
-            </CardFooter>
-          </Card>
-          
+          {latestPropertiesForSale.map((property) => (
+            <PropertyCard
+              key={property._id}
+              property={property}
+              handleOnClick={handleOnClick}
+            />
+          ))}
         </section>
-      </section> */}
+      </section>
+
+      <section className="flex flex-col px-10 lg:px-20 py-10 lg:py-24 m-auto gap-10 items-center justify-center bg-slate-100">
+        <h2>Latest House on Rent</h2>
+        <p className="text-[#889099] text-center">
+          Checkout latest houses, apartment and commercial properties on Rent.
+        </p>
+        <section className="flex gap-4 container overflow-x-auto hide-scroll-bar ">
+          {latestPropertiesForRent.map((property) => (
+            <PropertyCard
+              key={property._id}
+              property={property}
+              handleOnClick={handleOnClick}
+            />
+          ))}
+        </section>
+      </section>
       <Footer />
     </div>
   );
