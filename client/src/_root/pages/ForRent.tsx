3f3096ebbd1ac3e4cd8aecf6e235a9f1d48673e6
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2024-01-18 00:13:29 +0300

    Replicate the content from ForSale

--- client/src/_root/pages/ForRent.tsx
+++ client/src/_root/pages/ForRent.tsx
@@ -3,19 +3,23 @@
  */
 
 import PropertyCard from "@/components/shared/PropertyCard";
-import TopBar from "@/components/shared/TopBar";
-import Footer from "@/components/shared/Footer";
+
 import { useNavigate, useLocation } from "react-router-dom";
 
 
 import { fetchData } from "@/services/api";
 import { useEffect, useState } from "react";
 import { PropertyInterface } from "@/_dashboard/_components/ListingTable";
+import { Button } from "@/components/ui/button";
+
+import { dummyData } from "../data";
 
 const ForRent = () => {
-  const [propertiesForRent, setPropertiesForRent] = useState<
+  const [propertiesForSale, setPropertiesForSale] = useState<
     PropertyInterface[]
   >([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const productsPerPage: number = 6;
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -24,39 +28,70 @@ const ForRent = () => {
     navigate(`${currentUrl}/${id}`);
   };
 
+  const handlePageChange = (page: number): void => {
+    setCurrentPage(page);
+  };
+
+  const totalProducts: number = propertiesForSale?.length || 0;
+  const totalPages: number = Math.ceil(totalProducts / productsPerPage);
+
+  // Calculate the index range of products to display for the current page
+  const indexOfLastProduct: number = currentPage * productsPerPage;
+  const indexOfFirstProduct: number = indexOfLastProduct - productsPerPage;
+  const currentProperties: PropertyInterface[] | undefined =
+    propertiesForSale?.slice(indexOfFirstProduct, indexOfLastProduct);
+
   useEffect(() => {
-    const fetchPropertyForRent = async () => {
+    const fetchPropertyForSale = async () => {
       try {
-        const res = await fetchData("property/for-rent");
-        setPropertiesForRent(res.properties);
+        // const res = await fetchData('property/for-sale');
+        // setPropertiesForSale(res.properties)
+        setPropertiesForSale(dummyData);
       } catch (error) {
         console.error(error);
       }
     };
-    fetchPropertyForRent();
+    fetchPropertyForSale();
   }, []);
 
   return (
-    <div className="w-full text-gray-700">
-      <TopBar textcolor="text-gray-700" />
-      <section className="flex flex-col gap-4 mt-10">
-        <h2>Properties For Rent In Kenya</h2>
-        <section className="container mx-auto p-4 flex flex-wrap ">
-          {propertiesForRent.length ? propertiesForRent.map((property) => (
-            <PropertyCard
-              key={property._id}
-              property={property}
-              handleOnClick={handleOnClick}
-            />
-          )):<>
-          <p>No properties for rent.</p>
-          </>}
+    <div className="flex flex-col w-full gap-6">
+      {/* <SearchForm /> */}
+      <section className="flex flex-col mx-auto gap-4 mt-10">
+        <h2 className="">Properties For Rent In Kenya</h2>
+        <section className="flex flex-row gap-6  flex-wrap ">
+          {propertiesForSale.length ? (
+            currentProperties.map((property) => (
+              <PropertyCard
+                key={property._id}
+                property={property}
+                handleOnClick={handleOnClick}
+              />
+            ))
+          ) : (
+            <>
+              <p> No properties for sale</p>
+            </>
+          )}
         </section>
-       
       </section>
-      <Footer />
+      <section className=" ">
+        <ul className="flex flex-row justify-center gap-4">
+          {Array.from({ length: totalPages }, (_, i) => (
+            <li
+              key={i + 1}
+              className={`${
+                currentPage === i + 1 ? "bg-primary text-white" : ""
+              }`}
+            >
+              <Button variant="ghost" onClick={() => handlePageChange(i + 1)}>
+                {i + 1}
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 };
-
 export default ForRent;
